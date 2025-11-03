7489a16d779788e70d814d5ddd3ea0481e8d6dc7
peconnally <61524425+peconnally@users.noreply.github.com>
2025-11-03 14:44:20 -0500

    Update menu.h

--- include/menu.h
+++ include/menu.h
@@ -3,5 +3,10 @@
 //
 #ifndef MENU_H
 #define MENU_H
+#include <string>
+#include <vector>
+#include "csv_parser.h"
 
+bool isInteger(const std::string& str);
+void runMenu(std::vector<Row> data);
 #endif //MENU_H
