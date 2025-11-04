f004cd7f33a858ce24251592ef18ee80ab246cbd
peconnally <61524425+peconnally@users.noreply.github.com>
2025-11-03 23:42:31 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,18 @@
 #include "heap_sort.h"
 #include "menu.h"
 #include "merge_sort.h"
+#include <iostream>
 
 //main should be only a few lines, we're going to do the menu in a runMenu(data) function
 int main(){
     std::string fileName = "../data/airlines.csv";
     //Load data in this line
     std::vector<Row> data = loadCSV(fileName);
+    std::cout << "---------------------------------------------\nFLIGHT DELAY ANALYZER\n---------------------------------------------\n";
     //run menu in this line, passing in the data
-    runMenu(data);
-    return 0;
+    while (true) {
+        if (runMenu(data) == 1) {
+            return 0;
+        }
+    }
 }
