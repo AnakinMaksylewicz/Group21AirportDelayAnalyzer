82d51b9e81cc7e003b00fbb68e1a6c85426bb68b
DennisUz <dennisuzun18@gmail.com>
2025-11-04 00:03:26 -0500

    heap sort implemented

--- include/heap_sort.h
+++ include/heap_sort.h
@@ -1,8 +1,18 @@
 //
-// Created by anakin on 10/15/2025.
+// File created by anakin on 10/15/2025.
+// Implemented by dennis 11/3/2025
 //
 
 #ifndef HEAP_SORT_H
 #define HEAP_SORT_H
+
+#include <vector>
+#include "csv_parser.h"
+
+//compare function for the same reasoning as merge_sort.h
+using compareFunction = bool(*)(const Row&, const Row&);
+
+void heapSort(std::vector<Row>& data, compareFunction comp);
+
 
 #endif //HEAP_SORT_H
--- src/heap_sort.cpp
+++ src/heap_sort.cpp
@@ -1,3 +1,47 @@
 //
-// Created by anakin on 10/15/2025.
+// File created by anakin on 10/15/2025.
+// Implemented by dennis 11/3/2025
 //
+
+#include "heap_sort.h"
+using namespace std;
+
+void heapify(vector<Row>& data, int n, int i, compareFunction comp) {
+    // largest initialized as root
+    int largest = i;
+    // left and right indexes
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    // left child greater than root
+    if (left < n && comp(data[largest], data[left])) {
+        largest = left;
+    }
+    // right child greater than largest so far
+    if (right < n && comp(data[largest], data[right])) {
+        largest = right;
+    }
+    // largest is not root
+    if (largest != i) {
+        swap(data[largest], data[i]);
+        //heapify rest of subtree
+        heapify(data, n, largest, comp);
+    }
+}
+
+void heapSort(std::vector<Row>& data, compareFunction comp) {
+    int n = data.size();
+
+    // build heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(data, n, i, comp);
+    }
+    // one by one element swaps
+    for (int i = n - 1; i >= 0; i--) {
+        // root -> end
+        swap(data[0], data[i]);
+
+        heapify(data, i, 0, comp);
+    }
+}
+
